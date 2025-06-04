892878f98875b67a5e75495059b978bf385ec41a
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-06-04 14:34:10 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/routerDashboard/",
   plugins: [vue(), tailwindcss()],
 });
